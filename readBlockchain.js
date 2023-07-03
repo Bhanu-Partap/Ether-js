8726a03abc3e552679cebed9eaddecc05930c3eb
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-03 12:20:25 +0530

    downgraded the ethers version

--- readBlockchain.js
+++ readBlockchain.js
@@ -1,4 +1,10 @@
-import { ethers } from "ethers";
-const provider = new ethers.provider.JsonRpcProvider(
-  `https://mainnet.infura.io/v3/9a8614ca9a11412aaab9734cdde0cd29`
+const { ethers } = require("ethers");
+const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/9a8614ca9a11412aaab9734cdde0cd29'
 );
+const queryBlockchain = async()=>{
+    const block = await provider.getBlockNumber();
+    console.log("current block number s :", block);
+
+}
+
+queryBlockchain();
