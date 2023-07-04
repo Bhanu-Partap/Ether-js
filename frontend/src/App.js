4b5b36b1cfbcedd1b4d1c33fea3b9a5f1a9242f3
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-04 11:05:23 +0530

    metamask pop up added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import abi from "./abi.json";
+import { ethers } from "ethers";
+import { useEffect} from "react"
 
 function App() {
+  const contractAddress = "0xDE1146AD18dDc00f45A578b7f6612Cbe8FD96De6";
+
+  useEffect(() => {
+    const abijson = abi;
+    const writeContract = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abijson, signer);
+      await contract.setValue(2);
+    };
+    writeContract();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="helo">hello</div>
+    </>
   );
 }
 
